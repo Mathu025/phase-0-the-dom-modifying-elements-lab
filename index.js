37621868d54a37bebcecb44c20b1a17d12d99575
Mathu025 <mathualex72@gmail.com>
2025-06-20 17:38:40 +0300

    Complete DOM Nodes Lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const element = document.querySelector('main#main').remove()
+const newHeader = document.createElement('h1')
+newHeader.id= 'victory';
+newHeader.textContent = 'Alex Mathu is the champion'
+document.body.append(newHeader)
+
+
+
+
